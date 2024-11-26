e8e046529a252e846158626b609026fc9c1cdda9
Matt <bmatt0835@gmail.com>
2024-11-26 13:05:37 +0000

    Added Link round each item in featuredItems Component

--- ripple/src/app/page.js
+++ ripple/src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { getItems, getFoodBanks } from "./functions/api";
 import { useEffect, useState } from "react";
 import FeaturedItems from "@/components/Home/FeaturedItems";
@@ -10,32 +10,37 @@ export default function Home() {
 
   useEffect(() => {
     getItems()
-      .then((FetchedItems) => setFeaturedItems(FetchedItems))
-      .catch((err) => console.error("Error Fetching Featured Items - HOME", err))
+      .then((FetchedItems) => {
+        setFeaturedItems(FetchedItems.slice(0, 10));
+      })
+      .catch((err) =>
+        console.error("Error Fetching Featured Items - HOME", err)
+      )
       .finally(() => setLoadingItems(false));
   });
 
   return (
-    <section className="bg-transparent min-h-screen">
+    <section className="bg-transparent min-h-screen font-Quicksand">
       <header className="text-center py-8 px-4 md:px-8">
         <h1 className="text-3xl md:text-5xl font-bold text-textPrimary-light dark:text-textPrimary-dark">
-          Welcome to Ripple
+          WELCOME TO RIPPLE
         </h1>
         <p className="mt-4 text-base md:text-lg text-gray-600 dark:text-gray-400">
           Connect, Donate, and Support Communities in Need.
         </p>
-        <div  className="flex-grow border-t border-white mx-4 opacity-50 mt-5"></div>
+        <div className="flex-grow border-t border-white mx-4 opacity-50 mt-5"></div>
       </header>
       <main className="flex items-center justify-center flex-1 flex-col flex-grow px-4">
-      <h2 className="text-2xl sm:text-3xl font-semibold font-Quicksand text-textPrimary-light dark:text-textPrimary-dark text-center mb-6 px-4 break-words max-w-full">
-        CHECK OUT THE LATEST ITEMS FROM THE COMMUNITY
-      </h2>
+        <h2 className="text-2xl sm:text-3xl font-semibold font-Quicksand text-textPrimary-light dark:text-textPrimary-dark text-center mb-6 px-4 break-words max-w-full">
+          CHECK OUT THE LATEST ITEMS FROM THE COMMUNITY
+        </h2>
         {loadingItems ? (
           <p className="text-center text-gray-500">Loading featured items...</p>
         ) : (
           <FeaturedItems items={featuredItems} />
         )}
       </main>
+      <div className="flex-grow border-t border-white mx-4 px-4 opacity-50 mt-5"></div>
     </section>
   );
 }
--- ripple/src/components/Home/FeaturedItems.jsx
+++ ripple/src/components/Home/FeaturedItems.jsx
@@ -1,5 +1,6 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Link from "next/link";
 
 export default function FeaturedItems({ items }) {
   return (
@@ -17,26 +18,34 @@ export default function FeaturedItems({ items }) {
         className="px-4 bg-transparent"
       >
         {items.map((item) => (
-          <div
+          <Link
             key={item.item_id}
-            className="p-2 bg-transparent shadow-md rounded-lg"
+            href={`/items/${item.item_id}`} // Dynamic route for each item
+            passHref
           >
-            <div className="max-w-[1200px] h-[350px] lg:h-[500px] relative mx-auto">
-              <img
-                src={item.image_url}
-                alt={item.item_name}
-                className="w-full h-full object-cover rounded-md"
-              />
-              <div className="absolute bottom-0 left-0 right-0 p-4">
-                <div className="bg-white bg-opacity-80 p-2 rounded-lg font-Quicksand">
-                  <h3 className="text-lime-500 text-xl sm:text-2xl font-semibold">
-                    {item.item_name.toUpperCase()}
-                  </h3>
-                  <p className="text-textPrimary-light text-base sm:text-lg">{item.description}</p>
+            <div
+              key={item.item_id}
+              className="p-2 bg-transparent shadow-md rounded-lg"
+            >
+              <div className="max-w-[1200px] h-[350px] lg:h-[500px] relative mx-auto">
+                <img
+                  src={item.image_url}
+                  alt={item.item_name}
+                  className="w-full h-full object-cover rounded-md"
+                />
+                <div className="absolute bottom-0 left-0 right-0 p-4">
+                  <div className=" bg-white bg-opacity-80 p-2 rounded-lg font-Quicksand">
+                    <h3 className="text-lime-500 text-xl sm:text-2xl font-semibold">
+                      {item.item_name.toUpperCase()}
+                    </h3>
+                    <p className="text-textPrimary-light text-base sm:text-lg">
+                      {item.description}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </Carousel>
     </section>
